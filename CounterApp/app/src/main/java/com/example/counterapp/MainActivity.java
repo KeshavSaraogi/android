dc9d0d22af17978d5cb6231d1a251d6e3d7dece4
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-23 19:02:31 +0530

    CounterApp
    -- declaring textViews and Buttons
    -- creating onClick function
    -- increasing counter when onClick

--- CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
+++ CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView counterText, welcomeText;
+    Button button;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counterText = findViewById(R.id.textView);
+        welcomeText = findViewById(R.id.welcomeText);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterText.setText("" + increaseCount());
+            }
+        });
+    }
+    public int increaseCount() {
+        return counter = counter + 1;
     }
 }
