5951dce2267f4ea9b7b9ea17a21d9e100cbdedd3
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-23 20:00:50 +0530

    UnitConverterApp
    -- onClickListenerEvent
    -- fixing the UI by limiting to 2 decimal places

--- UnitConverterApp/app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ UnitConverterApp/app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.unitconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView welcomeText, resultText;
+    Button convertButton;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcomeText = findViewById(R.id.welcomeText);
+        resultText = findViewById(R.id.resultText);
+        inputText = findViewById(R.id.inputText);
+        convertButton = findViewById(R.id.button);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double kilograms = Double.parseDouble(inputText.getText().toString());
+                double result = convertKiloToPounds(kilograms);
+                resultText.setText("" + result + "LB");
+            }
+        });
+    }
+
+    public double convertKiloToPounds(double kilos){
+        double result = kilos * 220.5;
+        result = Math.round(result);
+        result = result / 100;
+        return result;
     }
 }
