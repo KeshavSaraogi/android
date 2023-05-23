8db11512c02a036272c2e8e57c6121a3f5cbb287
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-23 10:19:08 +0530

    Update MainActivity.java
    -- defining a textView and changing its background color in the main activity

--- WidgetsApp/app/src/main/java/com/example/widgetapp/MainActivity.java
+++ WidgetsApp/app/src/main/java/com/example/widgetapp/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.widgetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+        textView.setBackgroundColor(Color.RED);
     }
 }
