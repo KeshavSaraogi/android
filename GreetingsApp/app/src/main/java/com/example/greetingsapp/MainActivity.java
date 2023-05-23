cc0a07534688ee44f15a4b9dfd7dee6c024fd5aa
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-23 17:21:11 +0530

    functionality
    -- displaying user input text in a Toast message

--- GreetingsApp/app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ GreetingsApp/app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.greetingsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this,"Hello " + name , Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
