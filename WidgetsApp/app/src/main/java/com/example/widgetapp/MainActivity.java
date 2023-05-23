dd51bb6d3f4b4a3723f180c4c79c6879006dd0e7
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-23 11:59:13 +0530

    activity_main
    -- textView manipulation
    -- editText data
    -- button and click events

--- WidgetsApp/app/src/main/java/com/example/widgetapp/MainActivity.java
+++ WidgetsApp/app/src/main/java/com/example/widgetapp/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.text);
         textView.setBackgroundColor(Color.RED);
+
+        editText = findViewById(R.id.editText);
+        String input = editText.getText().toString();
+
+        button = findViewById(R.id.button);
+
+        //handling the on-click listener event
+        button.setOnClickListener(new View.OnClickListener() {
+            //the action performed when the button is clicked
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You Clicked The Button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
