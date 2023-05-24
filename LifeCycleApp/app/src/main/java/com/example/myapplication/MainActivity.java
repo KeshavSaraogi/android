7d054bfda5bba97cac5e91eaf00de01eb365b9a5
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-24 18:28:21 +0530

    Update MainActivity.java
    -- understanding how the different functions of the lifecycle are presented and used

--- LifeCycleApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ LifeCycleApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,
+                "onCreate() is called",
+                Toast.LENGTH_LONG).
+                show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,
+                "onStart() is called",
+                Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,
+                        "onResume() is called",
+                        Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,
+                        "onRestart() is called",
+                        Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,
+                        "onPause() is called",
+                        Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,
+                        "onStop() is called",
+                        Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,
+                        "onDestroy() is called",
+                        Toast.LENGTH_LONG)
+                .show();
     }
 }
