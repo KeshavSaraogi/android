09a4149eedafd6c470236528981cadf508de4964
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-26 14:06:03 +0530

    widgets
    -- check boxes and toast messages

--- CreatingMenu/app/src/main/java/com/example/creatingmenu/MainActivity.java
+++ CreatingMenu/app/src/main/java/com/example/creatingmenu/MainActivity.java
@@ -5,13 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    CheckBox firstBox, secondBox;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstBox = findViewById(R.id.firstCheckBox);
+        secondBox = findViewById(R.id.secondCheckBox);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firstBox.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "First Box Is Clicked", Toast.LENGTH_SHORT).show();
+                }
+                if (secondBox.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "Second Box Is Clicked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
