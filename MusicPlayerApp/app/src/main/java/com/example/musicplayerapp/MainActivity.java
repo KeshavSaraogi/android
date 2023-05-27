4a814e0f7a15959acf1d9efc3d20d15f921f9cd1
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-27 16:02:37 +0530

    widgets and variables
    -- declaring and initialising widgets and variables
    -- adding MediaPlayer library to control the song in Android Resource Directory

--- MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/MainActivity.java
+++ MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.musicplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int once = 0;
+    double startTime = 0;
+    double finalTime = 0;
+    int forwardTime = 10000; //milliseconds
+    int backwardTime = 10000; // milliseconds
+
+    TextView timeLeft, songPlaying;
+    Button playArrowButton, fastForwardButton, fastRewindButton, pauseButton;
+    SeekBar songProgress;
+
+    MediaPlayer mediaPlayer;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeLeft = findViewById(R.id.timeLeft);
+        songPlaying = findViewById(R.id.songPlaying);
+        playArrowButton = findViewById(R.id.playArrowButton);
+        fastForwardButton = findViewById(R.id.fastForwardButton);
+        fastRewindButton = findViewById(R.id.fastRewindButton);
+        pauseButton = findViewById(R.id.pauseButton);
+        songProgress = findViewById(R.id.songProgress);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.austronaut);
+        songProgress.setClickable(false);
     }
 }
