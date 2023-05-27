4f6adfcc4957a7eb9e6cddf959fe9fa572571725
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-05-27 09:08:13 +0530

    Spinner
    -- spinner, adapter and onClick event

--- CreatingMenu/app/src/main/java/com/example/creatingmenu/MainActivity.java
+++ CreatingMenu/app/src/main/java/com/example/creatingmenu/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     CheckBox firstBox, secondBox;
     Button button;
     RadioButton first, second;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         first = findViewById(R.id.with);
         second = findViewById(R.id.without);
+        spinner = findViewById(R.id.firstSpinner);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +47,28 @@ public class MainActivity extends AppCompatActivity {
                 if (second.isChecked()) {
                     Toast.makeText(getApplicationContext(), "Second Radio Button Clicked", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        //Setting up a spinner to display the data source
+        //With Spinner, there are three components:
+        // 1. Data Source  2. Adapter  3. Spinner
+
+        String[] courses = {"C", "C++", "Java", "Python", "Kotlin", "JS"};
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, courses);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        //on-click listening with spinners
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "You Selected " + courses[position], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
