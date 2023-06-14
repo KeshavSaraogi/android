ad5cb26395cfb37f6de45fe75898eaba1f3b89f7
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-06-14 18:55:38 +0530

    custom adapter
    -- creating a custom adapter
    -- learning and creating getView method and layoutInflator

--- WorldCup/app/src/main/java/com/example/worldcup/CustomAdapter.java
+++ WorldCup/app/src/main/java/com/example/worldcup/CustomAdapter.java
@@ -1,7 +1,68 @@
 package com.example.worldcup;
 
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CustomAdapter
-        extends ArrayAdapter<CountryModel> {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter<CountryModel> {
+
+    private ArrayList<CountryModel> countryArrayList;
+    Context context;
+
+    public CustomAdapter(ArrayList<CountryModel> data, Context context){
+        super(context, R.layout.world_cup_layout, data);
+        this.countryArrayList = data;
+        this.context = context;
+    }
+
+    private static class ViewHolder {
+        TextView country;
+        TextView cupWins;
+        ImageView flag;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //get data item for this position
+        CountryModel dataModel = getItem(position);
+
+        //check if the existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder;
+
+        final View result;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+            convertView = layoutInflater.inflate
+                    (R.layout.world_cup_layout, parent,false);
+
+            viewHolder.country = (TextView) convertView.findViewById(R.id.countryText);
+            viewHolder.cupWins = (TextView) convertView.findViewById(R.id.numberOfWins);
+            viewHolder.flag = (ImageView) convertView.findViewById(R.id.countryImage);
+
+            result = convertView;
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+        viewHolder.country.setText(dataModel.getCountryName());
+        viewHolder.cupWins.setText(dataModel.getCupWins());
+        viewHolder.flag.setImageResource(dataModel.getFlagImage());
+
+        return convertView;
+    }
 }
--- WorldCup/app/src/main/java/com/example/worldcup/MainActivity.java
+++ WorldCup/app/src/main/java/com/example/worldcup/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.worldcup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    private static CustomAdapter customAdapter;
+    ArrayList<CountryModel> dataModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listView.findViewById(R.id.listView);                               // listview       : adapter view
+
+        dataModel = new ArrayList<>();                                      // data model     : data source
+        dataModel.add(new CountryModel("Brazil", "5", R.drawable.brazil));
+        dataModel.add(new CountryModel("Germany", "4", R.drawable.germany));
+        dataModel.add(new CountryModel("France", "2", R.drawable.france));
+        dataModel.add(new CountryModel("Spain", "1", R.drawable.spain));
+        dataModel.add(new CountryModel("United Kingdom", "1", R.drawable.uk));
+        dataModel.add(new CountryModel("Saudi Arabia", "0", R.drawable.saudiarabia));
+        dataModel.add(new CountryModel("USA", "0", R.drawable.usa));
+
+        customAdapter = new CustomAdapter(dataModel, getApplicationContext());  // custom adapter : adapter
+
+        listView.setAdapter(customAdapter);
     }
 }
