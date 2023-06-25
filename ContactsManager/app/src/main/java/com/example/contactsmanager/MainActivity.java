6e2ad40ebda564ba6f004b6e07af2f63908e29b8
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-06-25 13:05:24 +0530

    adding a theme
    -- creating adapter and helper functions for main activity
    -- defining some new styles
    -- additional files

--- ContactsManager/app/src/main/java/com/example/contactsmanager/MainActivity.java
+++ ContactsManager/app/src/main/java/com/example/contactsmanager/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.contactsmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.contactsmanager.Adapter.ContactsAdapter;
+import com.example.contactsmanager.database.entity.Contact;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ContactsAdapter contactsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addAndEditContacts(boolean b, Contact contact, int positions) {
     }
 }
