696b7e289e380d8c0eeefccf7022ba9e83dad316
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-03 15:10:45 +0530

    Create Result.java

---
+++ MoviesApplication/app/src/main/java/com/example/moviesapplication/Model/Result.java
@@ -0,0 +1,99 @@
+package com.example.moviesapplication.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Result implements Parcelable {
+
+    //Variables
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("results")
+    @Expose
+    private List<Movie> results;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+
+    //Getters and Setters
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    //Constructor - Empty
+    public Result() {}
+
+    //Constructor - Parcelable
+    public Result(Parcel p) {
+        this.page = ((Integer) p.readValue((Integer.class.getClassLoader())));
+        this.totalResults = ((Integer) p.readValue((Integer.class.getClassLoader())));
+        this.totalPages = ((Integer) p.readValue((Integer.class.getClassLoader())));
+        p.readList(this.results, (com.example.moviesapplication.Model.Movie.class.getClassLoader()));
+    }
+
+    public final static Parcelable.Creator<Result> CREATOR = new Creator<Result>() {
+        @Override
+        public Result createFromParcel(Parcel source) {
+            return new Result(source);
+        }
+
+        @Override
+        public Result[] newArray(int size) {
+            return new Result[size];
+        }
+    };
+
+    //Parcel Interface Functions
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeValue(page);
+        dest.writeValue(totalPages);
+        dest.writeValue(totalResults);
+        dest.writeList(results);
+    }
+}
