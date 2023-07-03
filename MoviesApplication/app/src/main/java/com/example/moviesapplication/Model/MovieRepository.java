4d7a2caab8a1d49e295069fb0bf26c072126850c
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-03 16:41:34 +0530

    api call and retrofit
    -- creating an API call interface method for popular movies
    -- creating a retrofit instance (singleton DP)
    -- storing the data received from the MovieDataService

---
+++ MoviesApplication/app/src/main/java/com/example/moviesapplication/Model/MovieRepository.java
@@ -0,0 +1,49 @@
+package com.example.moviesapplication.Model;
+
+import android.app.Application;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.moviesapplication.R;
+import com.example.moviesapplication.Service.MovieDataService;
+import com.example.moviesapplication.Service.RetrofitInstance;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MovieRepository {
+
+    private ArrayList<Movie> movies = new ArrayList<>();
+    private MutableLiveData<ArrayList<Movie>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public MovieRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<ArrayList<Movie>> getMutableLiveData() {
+        MovieDataService movieDataService = RetrofitInstance.getService();
+
+        Call<Result> call = movieDataService.getPopularMovies(application.getApplicationContext().getString(R.string.api_key));
+        call.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                Result result = response.body();
+
+                if (result != null && result.getResults() != null) {
+                    movies = (ArrayList<Movie>) result.getResults();
+                    mutableLiveData.setValue(movies);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+
+            }
+        });
+        return mutableLiveData;
+    }
+}
---
+++ MoviesApplication/app/src/main/java/com/example/moviesapplication/Service/MovieDataService.java
@@ -0,0 +1,15 @@
+package com.example.moviesapplication.Service;
+
+import com.example.moviesapplication.Model.Result;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieDataService {
+
+    //BASE URL - https://api.themoviedb.org/3/
+    //ENDPOINT - tv/popular?api_key=5363cb14781680ea2ede9e8174ec6a8a
+    @GET("tv/popular")
+    Call<Result> getPopularMovies(@Query("api_key") String apiKey);
+}
