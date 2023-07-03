02a4ba7ca380a72f6f33657d9228ac44cc0def6a
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-03 21:33:46 +0530

    MovieAdapter
    -- creating an adapter class extending the View Holder class

---
+++ MoviesApplication/app/src/main/java/com/example/moviesapplication/Adapter/MovieAdapter.java
@@ -0,0 +1,78 @@
+package com.example.moviesapplication.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.moviesapplication.Model.Movie;
+import com.example.moviesapplication.R;
+import com.example.moviesapplication.View.MovieActivity;
+import com.example.moviesapplication.databinding.MovieItemBinding;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private Context context;
+    private ArrayList<Movie> movieArrayList;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movieArrayList) {
+        this.context = context;
+        this.movieArrayList = movieArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        MovieItemBinding movieItemBinding = DataBindingUtil.inflate
+                (LayoutInflater.from(
+                        parent.getContext()),
+                        R.layout.movie_item,
+                        parent,
+                        false
+                );
+        return new MovieViewHolder(movieItemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movieArrayList.get(position);
+        holder.movieItemBinding.setMovie(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieArrayList.size();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder{
+        private MovieItemBinding movieItemBinding;
+
+        public MovieViewHolder(MovieItemBinding movieItemBinding) {
+            super(movieItemBinding.getRoot());
+            this.movieItemBinding = movieItemBinding;
+
+            movieItemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if (position != RecyclerView.NO_POSITION) {
+                        Movie movieSelected = movieArrayList.get(position);
+                        Intent intent = new Intent(context, MovieActivity.class);
+                        intent.putExtra("name", movieSelected);
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+    }
+
+}
