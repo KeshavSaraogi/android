37314cfe15894b4dcb0b545efe1129121d5a06d7
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-04 18:53:42 +0530

    retrofit and utilities
    -- creating a retrofit interface and an api call
    -- creating utilities class

--- Movie/app/src/main/java/com/example/movie/model/Movie.java
+++ Movie/app/src/main/java/com/example/movie/model/Movie.java
@@ -3,7 +3,6 @@ package com.example.movie.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-//8d15b691e15005e2611f4b96b4c51bfd
 public class Movie {
 
     @SerializedName("id")
---
+++ Movie/app/src/main/java/com/example/movie/util/Utilities.java
@@ -0,0 +1,7 @@
+package com.example.movie.util;
+
+public class Utilities {
+
+    public static String API_KEY = "8d15b691e15005e2611f4b96b4c51bfd";
+    public static String BASE_URL = "https://api.themoviedb.org/3/";
+}
