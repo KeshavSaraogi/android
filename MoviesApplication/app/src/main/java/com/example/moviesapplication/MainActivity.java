3688d32c3f4a7193bbd15ea742103606ea0f5ee4
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-05 13:12:39 +0530

    Work Manager
    -- initial project setup

--- MoviesApplication/app/src/main/java/com/example/moviesapplication/MainActivity.java
+++ MoviesApplication/app/src/main/java/com/example/moviesapplication/MainActivity.java
@@ -33,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        
         getSupportActionBar().setTitle("Movies");
 
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
