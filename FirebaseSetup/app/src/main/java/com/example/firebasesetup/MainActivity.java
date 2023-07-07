23a902eed577e7805e7dc9ab28df0b387b9bb4bb
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-07 12:36:57 +0530

    writing data to firebase
    -- updating data to firebase

--- FirebaseSetup/app/src/main/java/com/example/firebasesetup/MainActivity.java
+++ FirebaseSetup/app/src/main/java/com/example/firebasesetup/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.firebasesetup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("Gold").child("GoldPrice").child("price");
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextNumber);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.setValue(editText.getText().toString());
+            }
+        });
+
+        //writing data to firebase
     }
 }
