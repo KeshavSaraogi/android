6d73eb5c6ec8afc3947f5d1f65f6535079b35efd
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-07 12:52:53 +0530

    reading values
    -- reading values from firebase

--- FirebaseSetup/app/src/main/java/com/example/firebasesetup/MainActivity.java
+++ FirebaseSetup/app/src/main/java/com/example/firebasesetup/MainActivity.java
@@ -1,34 +1,56 @@
 package com.example.firebasesetup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     Button button;
     EditText editText;
+    TextView gold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference("Gold").child("GoldPrice").child("price");
+        mDatabase = FirebaseDatabase.getInstance().getReference("gold").child("price");
         button = findViewById(R.id.button);
         editText = findViewById(R.id.editTextNumber);
+        gold = findViewById(R.id.gold);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDatabase.setValue(editText.getText().toString());
+            }
+        });
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //When any change occurs in the database
+                gold.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
