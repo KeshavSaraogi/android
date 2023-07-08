74f4326ce0ef78e34be0a3b27b24b9153197774a
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-08 16:45:02 +0530

    Custom Objects
    -- retrieving custom objects
    -- converting documents retrieved into objects

--- FirestoreApp/app/src/main/java/com/example/firestoreapp/MainActivity.java
+++ FirestoreApp/app/src/main/java/com/example/firestoreapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +14,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,10 +34,11 @@ import java.util.Map;
 
     private EditText nameET, emailET;
     private Button saveInfo, readInfo, updateInfo, deleteInfo;
-    private TextView text, display;
+    private TextView textView, display;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference friendsReference = db.collection("Users").document("Friends");
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +56,15 @@ import java.util.Map;
         saveInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveDataToFireStore();
+                //saveDataToFireStore();
+                saveDataToDocument();
             }
         });
         readInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                readData();
+                //readData();
+                getAllDocumentsInCollection();
             }
         });
         updateInfo.setOnClickListener(new View.OnClickListener() {
@@ -75,17 +82,32 @@ import java.util.Map;
         });
     }
 
+     private void saveDataToDocument() {
+        String name = nameET.getText().toString();
+        String email = nameET.getText().toString();
+
+        Friends friend = new Friends(name, email);
+        collectionReference.add(friend);
+
+     }
+
      private void saveDataToFireStore() {
         String name = nameET.getText().toString().trim();
         String email = emailET.getText().toString().trim();
 
-        Map<String, Object> data = new HashMap<>();
-        data.put(KEY_NAME, name);
-        data.put(KEY_EMAIL, email);
+         Friends friendOne = new Friends();
+         friendOne.setName(name);
+         friendOne.setEmail(email);
+
+
+
+//        Map<String, Object> data = new HashMap<>();
+//        data.put(KEY_NAME, name);
+//        data.put(KEY_EMAIL, email);
 
         db.collection("Users")
                 .document("Friends")
-                .set(data)
+                .set(friendOne)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
@@ -125,33 +147,55 @@ import java.util.Map;
              }
          });
      }
-
      private void deleteData() {
         friendsReference.update(KEY_NAME, FieldValue.delete());
         friendsReference.update(KEY_EMAIL,FieldValue.delete());
      }
-
      private void deleteAll() {
         friendsReference.delete();
+     }
+
+     private void getAllDocumentsInCollection() {
+        collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                String data = "";
+
+                //Looping through all the documents in the collection
+                for (QueryDocumentSnapshot snapshots: queryDocumentSnapshots) {
+                    //Log.v("TAGY",snapshots.getString(KEY_NAME));
+                    //Log.v("TAGY",snapshots.getString(KEY_EMAIL));
+
+                    //Transforming snapshots into Friends
+                    Friends friend = snapshots.toObject(Friends.class);
+                    data += "Name :" + friend.getName() + "Email: " + friend.getEmail();
+                }
+                textView.setText(data);
+            }
+        });
      }
 
      @Override
      protected void onStart(){
         super.onStart();
+        getAllDocumentsInCollection();
 
         //listening all the time during the app lifecycle
-         friendsReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-             @Override
-             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                 if (error != null){
-                     Toast.makeText(MainActivity.this, "Error Found",Toast.LENGTH_SHORT).show();
-                 }
-                 if (value != null && value.exists()) {
-                     String name = value.getString(KEY_NAME);
-                     String email = value.getString(KEY_EMAIL);
-                     display.setText("Username: " + name + "\nEmail: " + email);
-                 }
-             }
-         });
+//         friendsReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//             @Override
+//             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//                 if (error != null){
+//                     Toast.makeText(MainActivity.this, "Error Found",Toast.LENGTH_SHORT).show();
+//                 }
+//                 if (value != null && value.exists()) {
+//
+//                     Friends friend = value.toObject(Friends.class);
+////                     String name = value.getString(KEY_NAME);
+////                     String email = value.getString(KEY_EMAIL);
+//
+//                     display.setText("Username: " + friend.getName() + "\nEmail: " + friend.getEmail());
+//                 }
+//             }
+//         });
      }
  }
